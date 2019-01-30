e0169c6748987e7f2996fd5f2d6bb4bb328147c0
onlyhiero <shawn.descamps@gmail.com>
2019-01-30 17:23:06 +0100

    tout est finie yesssss

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+#10.times do
+ # user = User.create!(first_name: Faker::Cat.name, last_name: Faker::Color.color_name, email: Faker::Internet.email)
+#end
+
+#Comment.delete_all
+#Article.delete_all
+#Category.delete_all
+
+=begin
+10.times do
+	category = Category.create!(name: Faker::Book.genre)
+end
+
+25.times do
+  article = Article.create!(title: Faker::Book.title, content: Faker::WorldOfWarcraft.quote, user_id: rand(1..23), category_id: rand(41..50))
+end
+
+15.times do
+	comment = Comment.create!(content: Faker::Community.quotes, article_id: rand(26..50), user_id: rand(1..23))
+end
+=end
+
+25.times do
+	like = Like.create!(article_id: rand(26..50), user_id: rand(1..23))
+end
